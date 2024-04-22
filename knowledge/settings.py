512026eb202bca6eed0f16b70f2e8f459c99f159
Kassahun-Addis <kassahunaddiss@outlook.com>
2024-04-22 22:04:35 +0300

    pushed

--- knowledge/settings.py
+++ knowledge/settings.py
@@ -32,12 +32,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'daveyetadulij@gmail.com'
-# EMAIL_HOST_PASSWORD = '21mariyamenate'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'daveyetadulij@gmail.com'
+EMAIL_HOST_PASSWORD = '21mariyamenate'
 
 
 # Application definition
