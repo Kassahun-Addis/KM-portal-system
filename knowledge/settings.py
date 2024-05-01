a0654569a5563bd8c513ea65a5d57ff9bf3bf25a
Kassahun-Addis <kassahunaddiss6@gmail.com>
2024-05-02 02:09:03 +0300

    update onrender

--- knowledge/settings.py
+++ knowledge/settings.py
@@ -72,7 +72,9 @@ ROOT_URLCONF = 'knowledge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')  ],
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates')  ],
+        'DIRS': [r'D:\1 KM\KM portal system\knowledge\knowledge_portal\templates' ],
+        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,7 +127,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'EAT'
 
 USE_I18N = True
 
