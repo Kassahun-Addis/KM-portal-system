a7cf04e9ab1d249dac001f2e44c14ce66bb6d294
Kassahun-Addis <kassahunaddiss6@gmail.com>
2024-05-02 14:29:52 +0300

    update

--- knowledge/settings.py
+++ knowledge/settings.py
@@ -73,7 +73,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': [os.path.join(BASE_DIR, 'templates')  ],
-        'DIRS': [r'D:\1 KM\KM portal system\knowledge\knowledge_portal\templates' ],
+        # 'DIRS': [r'D:\1 KM\KM portal system\knowledge\knowledge_portal\templates' ],
+        'DIRS': [BASE_DIR, '/templates'],
         
         'APP_DIRS': True,
         'OPTIONS': {
